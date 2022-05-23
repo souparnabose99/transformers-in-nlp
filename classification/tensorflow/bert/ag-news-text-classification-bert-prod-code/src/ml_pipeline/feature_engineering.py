bffca8ada1e512f29a27f5c9daa6d0bff3418606
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-05-23 08:48:59 +0530

    Update feature_engineering.py

--- classification/tensorflow/bert/ag-news-text-classification-bert-prod-code/src/ml_pipeline/feature_engineering.py
+++ classification/tensorflow/bert/ag-news-text-classification-bert-prod-code/src/ml_pipeline/feature_engineering.py
@@ -1,1 +1,14 @@
 
+import ktrain
+from ktrain import text
+
+
+def perform_feature_engineering(ag_news_train_df, ag_news_test_df, max_length):
+    (X_train, y_train), (X_test, y_test), preprocessing_var = text.texts_from_df(train_df=ag_news_train_df,
+                                                                                 text_column='text',
+                                                                                 label_columns='label',
+                                                                                 val_df=ag_news_test_df,
+                                                                                 maxlen=max_length,
+                                                                                 preprocess_mode='bert')
+
+    return (X_train, y_train), (X_test, y_test), preprocessing_var
