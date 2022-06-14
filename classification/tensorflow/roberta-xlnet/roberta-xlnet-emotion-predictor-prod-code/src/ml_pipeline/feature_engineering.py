61e8e443ed79090720051f3e4712355694aec48f
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-06-14 08:23:00 +0530

    Update feature_engineering.py

--- classification/tensorflow/roberta-xlnet/roberta-xlnet-emotion-predictor-prod-code/src/ml_pipeline/feature_engineering.py
+++ classification/tensorflow/roberta-xlnet/roberta-xlnet-emotion-predictor-prod-code/src/ml_pipeline/feature_engineering.py
@@ -1,1 +1,11 @@
 
+import ktrain
+from ktrain import text
+
+
+def perform_data_preprocessing(transformer, X_train, y_train, X_test, y_test):
+    train = transformer.preprocess_train(X_train.to_list(), y_train.to_list())
+    val = transformer.preprocess_test(X_test.to_list(), y_test.to_list())
+    return train, val
+
+  
