3a21bad3f8119c742aa86e9d5846d4af55e04108
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-10 20:31:15 +0530

    Update albert.py

--- classification/tensorflow/albert-distilbert/albert-distilbert-predictor-on-dbpedia-prod-code/src/ml-pipeline/albert.py
+++ classification/tensorflow/albert-distilbert/albert-distilbert-predictor-on-dbpedia-prod-code/src/ml-pipeline/albert.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import ktrain
+from ktrain import text
+
+
+class ALBERT:
+
+    def __init__(self):
+        self.model_name = "albert-base-v1"
+        self.maxlen = 512
+        self.classes = ['Company',
+                        'EducationalInstitution',
+                        'Artist',
+                        'Athlete',
+                        'OfficeHolder',
+                        'MeanOfTransportation',
+                        'Building',
+                        'NaturalPlace',
+                        'Village',
+                        'Animal',
+                        'Plant',
+                        'Album',
+                        'Film',
+                        'WrittenWork']
+        self.batch_size = 6
+
+    def create_transformer(self):
+        return text.Transformer(self.model_name, self.maxlen, self.classes, self.batch_size)
 
