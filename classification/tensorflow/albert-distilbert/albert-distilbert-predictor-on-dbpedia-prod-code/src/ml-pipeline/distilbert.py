def131eb16e9f7425e29e8afc9436d0a7bf6e3fd
Souparna Bose <48193679+souparnabose99@users.noreply.github.com>
2022-07-17 21:44:56 +0530

    Update distilbert.py

--- classification/tensorflow/albert-distilbert/albert-distilbert-predictor-on-dbpedia-prod-code/src/ml-pipeline/distilbert.py
+++ classification/tensorflow/albert-distilbert/albert-distilbert-predictor-on-dbpedia-prod-code/src/ml-pipeline/distilbert.py
@@ -1,1 +1,30 @@
 
+import pandas as pd
+import ktrain
+from ktrain import text
+
+
+class DistilBERT:
+
+    def __init__(self):
+        self.model_name = "distilbert-base-uncased"
+        self.maxlen = 512
+        self.classes = ['Company',
+                        'EducationalInstitution',
+                        'Artist',
+                        'Athlete',
+                        'OfficeHolder',
+                        'MeanOfTransportation',
+                        'Building',
+                        'NaturalPlace',
+                        'Village',
+                        'Animal',
+                        'Plant',
+                        'Album',
+                        'Film',
+                        'WrittenWork']
+        self.batch_size = 16
+
+    def create_transformer(self):
+        return text.Transformer(self.model_name, self.maxlen, self.classes, self.batch_size)
+
